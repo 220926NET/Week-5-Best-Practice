9ea495771e9c30d4ee791b160018542250ae7144
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-25 17:33:57 -0400

    Initial Pokemon class

--- Models/Pokemon.js
+++ Models/Pokemon.js
@@ -1,5 +1,13 @@
-const myCar = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-  };
+class Pokemon {
+  constructor(name, hp, type, number, height, weight, move1, move2, entry) {
+    this.Name = name;
+    this.HP = hp;
+    this.Type = type;
+    this.Number = number;
+    this.Height = height;
+    this.Weight = weight;
+    this.Move1 = move1;
+    this.Move2 = move2;
+    this.Entry = entry;
+  }
+}
