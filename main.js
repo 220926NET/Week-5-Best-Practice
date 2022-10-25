8dfd57ea43d434c360113ffff2ca0c45b5ddaa73
rushayhays <rushayhays@outlook.com>
2022-10-25 15:39:46 -0500

    denis api and a broken api fetch call

---
+++ main.js
@@ -0,0 +1,8 @@
+fetch('https://pokeapi.co/api/v2/pokemon-species/ditto')
+  .then((response) => response.json())
+  .then((data) => console.log(data.flavor_text_entries[0].flavor_text));
+
+
+fetch('https://pokeapi.co/api/v2/pokemon-species/ditto')
+  .then((response) => response.json())
+  .then((data) => console.log(data.pokemon.name));
